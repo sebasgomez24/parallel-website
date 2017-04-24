78608a1dc1eb5beb5d7bb7c36d2acd9f1c461522
sebastian <sebasgomez24@gmail.com>
2017-04-24 14:30:01 -0700

    sebas: first commit

---
+++ src/Main_app/static/javascript/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+	/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+	particlesJS.load('particles-js', 'static/particles.json', function() {
+	  console.log('callback - particles.js config loaded');
+	});
+
+});
+	 
